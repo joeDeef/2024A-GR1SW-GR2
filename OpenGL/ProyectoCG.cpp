adb20cffa35269315b263f0f075fc4647a8d0ba6
JoelDefaz <joe.def2019@gmail.com>
2024-08-02 23:45:16 -0500

    adicción de modelos y transicion de la perspectiva de la camara

--- OpenGL/ProyectoCG.cpp
+++ OpenGL/ProyectoCG.cpp
@@ -19,24 +19,32 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+void setDisparoYPosicion();
 
 // settings
-const unsigned int SCR_WIDTH = 900;
-const unsigned int SCR_HEIGHT = 700;
+const unsigned int SCR_WIDTH = 1500;
+const unsigned int SCR_HEIGHT = 800;
 glm::vec3 lightPos(0.0f, 15.0f, 0.0f);
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(2.6f, 10.0f, -5.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-
+bool disparo = false;
+bool posicionDisparo = false;
+bool cambioCamara = false;
+int habilidad = 1;
+float desplazamiento = 0.003;
+int j = 0;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 glm::vec3 posicionNave = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 ultimaPosicion = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::mat4 projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.1f, 100.0f);
+
 
 struct CameraSettings {
     glm::vec3 position;
@@ -45,9 +53,9 @@ struct CameraSettings {
 };
 
 CameraSettings cameraSettings[] = {
-    {glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f)}, // Inicial
-    {glm::vec3(-5.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)}, // Lado Izquierdo
-    {glm::vec3(0.0f, 5.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f)}  // Arriba
+    {glm::vec3(0.0f, 1.5f, 2.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f)}, // Inicial
+    {glm::vec3(-5.0f, 1.5f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)}, // Lado Izquierdo
+    {glm::vec3(0.0f, 10.0f, 2.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f)}  // Arriba
 };
 
 float transitionDuration = 5.0f; // Duración de la transición en segundos
@@ -102,17 +110,19 @@ int main()
     Shader ourShader("shaders/shader_exercise16_mloading.vs", "shaders/shader_exercise16_mloading.fs");
     Shader lightingShader("shaders/shader_exercise14t4_materials.vs", "shaders/shader_exercise14t4_materials.fs");
 
-    // load models
-    // -----------
+    // Cargamos los modelos
     Model models[] = {
         Model("C:/Users/User/Documents/VisualStudio2022/OpenGL/OpenGL/model/immortalDemonic/demonio.obj"),
-        Model("C:/Users/User/Documents/VisualStudio2022/OpenGL/OpenGL/model/ovni/ovni.obj")
+        Model("C:/Users/User/Documents/VisualStudio2022/OpenGL/OpenGL/model/ovni/ovni.obj"),
+        Model("C:/Users/User/Documents/VisualStudio2022/OpenGL/OpenGL/model/nave/nave.obj"),
+        Model("C:/Users/User/Documents/VisualStudio2022/OpenGL/OpenGL/model/escenario/escenario.obj")
     };
 
     //Posicion Inicial de los modelos
     glm::vec3 movimiento[] = {
-        glm::vec3(0.9f,-0.8f,0.0f),
-        glm::vec3(0.0f,0.0f,0.0f)
+        glm::vec3(1.0f,1.5f,0.0f),
+        glm::vec3(0.0f,0.0f,0.0f),
+        glm::vec3(0.0f,-2.0f,0.0f),
     };    
 
     camera.MovementSpeed = 10; //Optional. Modify the speed of the camera
@@ -121,7 +131,6 @@ int main()
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        cambiarLugarCamara(camera, 0.0f, 0.0f, 0.0f);
         // per-frame time logic
         // --------------------
         float currentFrame = glfwGetTime();
@@ -137,42 +146,73 @@ int main()
 
         ourShader.use();
 
-        currentTime += 0.0001f; // Incrementa el tiempo de transición
+        currentTime += 0.001f; // Incrementa el tiempo de transición
         float t = currentTime / transitionDuration; // Normaliza el tiempo
 
         t = glm::clamp(t, 0.0f, 1.0f);
+        if (cambioCamara) {
+            j = 1;
+            currentTime = 0;
+            cambioCamara = false;
+        }
+            cameraPos = glm::mix(cameraSettings[j].position, cameraSettings[j+1].position, t);
+            glm::vec3 cameraFront = glm::mix(cameraSettings[j].front, cameraSettings[j+1].front, t);
+            glm::vec3 cameraUp = glm::mix(cameraSettings[j].up, cameraSettings[j+1].up, t);
 
-        cameraPos = glm::mix(cameraSettings[0].position, cameraSettings[2].position, t);
-        glm::vec3 cameraFront = glm::mix(cameraSettings[0].front, cameraSettings[2].front, t);
-        glm::vec3 cameraUp = glm::mix(cameraSettings[0].up, cameraSettings[2].up, t);
-
-        // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        ourShader.setMat4("projection", projection);
-        // Configurar la vista
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        ourShader.setMat4("view", view);
-
-        //std::cout << "Pos Camera: (" << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << ")" << std::endl;
-
-
+            // view/projection transformations
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            ourShader.setMat4("projection", projection);
+            glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+            ourShader.setMat4("view", view);
+            glm::mat4 model = glm::mat4(1.0f);
+        
         //Movimiento de la Nave
-        glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, posicionNave);
         model = glm::rotate(model, float(glfwGetTime()), glm::vec3(0.0f, 2.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
         ourShader.setMat4("model", model);
         models[1].Draw(ourShader);
-        
-        //Posoicon del Jefe
+        /*
+        if (tomoUnaHabilidad()) {
+            habilidad++;
+        }*/
+
+        float angulo = sin(glfwGetTime());
+        //movimiento Nave
+        if (disparo && habilidad > 0) {
+            model = glm::mat4(1.0f);
+            if (!posicionDisparo) {
+                ultimaPosicion = glm::vec3(float(glfwGetTime()), float(glfwGetTime()), 0.0f);
+                model = glm::translate(model, ultimaPosicion);
+                model = glm::translate(model, glm::vec3(-6.0f, -6.0f, 0.0f));
+                model = glm::rotate(model, angulo, glm::vec3(0.5f, 0.0f, 0.0f));
+            }
+            else {
+                model = glm::translate(model, glm::vec3(float(glfwGetTime()), 0.0f, 0.0f));
+                model = glm::translate(model, ultimaPosicion);
+                setDisparoYPosicion();
+            }
+            model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(0.025f, 0.025f, 0.025f));
+            ourShader.setMat4("model", model);
+            models[2].Draw(ourShader);
+        }
+
+        //Posicon del Jefe
         model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f,sin(glfwGetTime())/0.5 +0.5,0.0f));
         model = glm::translate(model, movimiento[0]);
-        model = glm::translate(model, movimiento[1]);
         model = glm::rotate(model, glm::radians(60.0f), glm::vec3(0.0f, -1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));
         ourShader.setMat4("model", model);
         models[0].Draw(ourShader);
+        
+        //Posicion del Escenario
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, movimiento[1]); // translate it down so it's at the center of the scene
+        //model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        ourShader.setMat4("model", model);
+        models[3].Draw(ourShader);
         
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -195,50 +235,59 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
         //camera.ProcessKeyboard(FORWARD, deltaTime);
         if (cameraPos == cameraSettings[1].position) {
-            posicionNave.y += 0.001;
+            posicionNave.y += desplazamiento;
         }
         else if (cameraPos == cameraSettings[2].position) {
-            posicionNave.z -= 0.001;
+            posicionNave.z -= desplazamiento;
         }
         else {
-            posicionNave.y += 0.001;
+            posicionNave.y += desplazamiento;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
         //camera.ProcessKeyboard(BACKWARD, deltaTime);
         if (cameraPos == cameraSettings[1].position) {
-            posicionNave.y -= 0.001;
+            posicionNave.y -= desplazamiento;
         }
         else if (cameraPos == cameraSettings[2].position) {
-            posicionNave.z += 0.001;
+            posicionNave.z += desplazamiento;
         }
         else {
-            posicionNave.y -= 0.001;
+            posicionNave.y -= desplazamiento;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         //camera.ProcessKeyboard(LEFT, deltaTime);
         if (cameraPos == cameraSettings[1].position) {
-            posicionNave.z -= 0.001;
+            posicionNave.z -= desplazamiento;
         }
         else if (cameraPos == cameraSettings[2].position) {
-            posicionNave.x -= 0.001;
+            posicionNave.x -= desplazamiento;
         }
         else {
-            posicionNave.x -= 0.001;
+            posicionNave.x -= desplazamiento;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
         //camera.ProcessKeyboard(RIGHT, deltaTime);
         if (cameraPos == cameraSettings[1].position) {
-            posicionNave.z += 0.001;
+            posicionNave.z += desplazamiento;
         }
         else if (cameraPos == cameraSettings[2].position) {
-            posicionNave.x += 0.001;
+            posicionNave.x += desplazamiento;
         }
         else {
-            posicionNave.x += 0.001;
+            posicionNave.x += desplazamiento;
         }
+    }
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+        disparo = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
+        posicionDisparo = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+        cambioCamara = true;
     }
 }
 
@@ -278,6 +327,13 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     camera.ProcessMouseScroll(yoffset);
 }
 
-void cambiarLugarCamara(Camera camera, float x, float y, float z) {
+bool tomoUnaHabilidad() {
+    return false;
+}
 
+void setDisparoYPosicion() {
+    if (true) {
+        disparo = false;
+        posicionDisparo = false;
+    }
 }
