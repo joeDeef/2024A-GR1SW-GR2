0c03a3896f684a1708ad49e0ddf83c4ccaa4e5e0
FRANCISCO ANDRES SANCHEZ GUERRERO <francisco.sanchez01@epn.edu.ec>
2024-08-04 14:35:31 -0500

    demonio

---
+++ OpenGL/demonioProyecto.cpp
@@ -0,0 +1,192 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/shader.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION 
+#include <learnopengl/stb_image.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// fireball position
+glm::vec3 demonio(1.0f, 0.0f, -0.3f);
+glm::vec3 ballPos(demonio.x, demonio.y + 0.37f, demonio.z + 0.37f);
+
+
+int main()
+{
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Exercise 16 Task 3", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    Shader ourShader("shaders/shader_exercise16_mloading.vs", "shaders/shader_exercise16_mloading.fs");
+
+    // load models
+    Model ourModel("D:/5/Computacion_Grafica/OpenGL/OpenGL/model/carved_stone_ball_hillhead_orkney/ball.obj");
+    Model secondModel("D:/5/Computacion_Grafica/OpenGL/OpenGL/model/immortalDemonic/demonio.obj");
+
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    camera.MovementSpeed = 5; // Optional. Modify the speed of the camera
+
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        processInput(window);
+
+        // update fireball position
+            if (ballPos.z > 4.0f) {
+                ballPos.z = demonio.z + 0.37f; // Reset position
+                ballPos.x = demonio.x;
+                ballPos.y = demonio.y + 0.37f;
+            }
+            else {
+                ballPos.z += deltaTime * 1.2f;
+            }
+            
+            demonio.x = (cos(glfwGetTime()/3.0f)) * 3.0f;
+
+        // render
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // don't forget to enable shader before setting uniforms
+        ourShader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        // render the fireball model
+        glm::mat4 fireballModelMatrix = glm::mat4(1.0f);
+        fireballModelMatrix = glm::translate(fireballModelMatrix, glm::vec3(ballPos)); // Move the fireball
+        fireballModelMatrix = glm::scale(fireballModelMatrix, glm::vec3(0.1f, 0.1f, 0.1f));	// scale it down
+        ourShader.setMat4("model", fireballModelMatrix);
+        ourModel.Draw(ourShader);
+
+        // render the second model
+        glm::mat4 secondModelMatrix = glm::mat4(1.0f);
+        secondModelMatrix = glm::translate(secondModelMatrix, glm::vec3(demonio)); // translate it to the right
+        secondModelMatrix = glm::scale(secondModelMatrix, glm::vec3(0.1f, 0.1f, 0.1f));	// scale it down
+        ourShader.setMat4("model", secondModelMatrix);
+        secondModel.Draw(ourShader);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
--- OpenGL/shaders/shader_exercise16_mloading.fs
+++ OpenGL/shaders/shader_exercise16_mloading.fs
@@ -1,28 +1,11 @@
 #version 330 core
 out vec4 FragColor;
+
 in vec2 TexCoords;
 
 uniform sampler2D texture_diffuse1;
-uniform sampler2D texture_specular1;
-uniform sampler2D texture_normal1;
-uniform sampler2D texture_height1;
 
 void main()
-{
-    // Lee el color de la textura difusa
-    vec4 diffuseColor = texture(texture_diffuse1, TexCoords);
-    // Lee el color de la textura especular
-    vec4 specularColor = texture(texture_specular1, TexCoords);
-    // Lee el color de la textura normal
-    vec4 normalColor = texture(texture_normal1, TexCoords);
-    // Lee el color de la textura de altura
-    vec4 heightColor = texture(texture_height1, TexCoords);
-
-    // Combina las texturas de alguna manera; aquí solo para ilustrar, puedes ajustar según sea necesario
-    vec4 finalColor = diffuseColor;
-    finalColor.rgb += specularColor.rgb * 0.2; // Por ejemplo, agrega un poco del color especular
-    finalColor.rgb += normalColor.rgb * 0.1;   // Agrega un poco del color normal
-    finalColor.rgb += heightColor.rgb * 0.05;  // Agrega un poco del color de altura
-
-    FragColor = finalColor;
+{    
+    FragColor = texture(texture_diffuse1, TexCoords);
 }
